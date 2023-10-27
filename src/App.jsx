2f913823dd2407aa0e7eda28df94f6ddf994fc6b
Raj Chimmalgi <chimmalgi@mgail.com>
2023-10-26 17:33:34 -0700

    display paragraphs based on input

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,40 @@
+import { useState } from "react";
+import data from "./data.js";
+import { nanoid } from "nanoid";
+
 const App = () => {
-  return <h2>Lorem Ipsum Starter</h2>;
+  const [count, setCount] = useState(1);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setText(data.slice(0, count));
+  };
+
+  return (
+    <section className="section-center">
+      <h4>tired of boring lorem ipsum?</h4>
+      <form onSubmit={handleSubmit} className="lorem-form">
+        <label htmlFor="paragraphs">Paragraphs</label>
+        <input
+          className="form-input"
+          type="number"
+          id="paragraphs"
+          value={count}
+          min="1"
+          max="8"
+          onChange={(e) => setCount(Number(e.target.value))}
+        />
+        <button className="btn" type="submit">
+          Generate
+        </button>
+      </form>
+      <article className="lorem-text">
+        {text.map((p) => (
+          <p key={nanoid()}>{p}</p>
+        ))}
+      </article>
+    </section>
+  );
 };
 export default App;
